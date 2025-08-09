aea54f4d40ab5d5e3d364922ed3c19257dd6c2b7
WatsonAndrade <watsonleidson@gmail.com>
2025-08-09 15:44:18 -0300

    feat: removendo front end antigo para dar inicio ao novo.

--- controle-financeiro/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { GastosComponent } from './gastos/gastos.component';
-import { ResumoAnualComponent } from './resumo-anual/resumo-anual.component';
-
-export const routes: Routes = [
-  { path: '', component: GastosComponent },
-  { path: 'resumo', component: ResumoAnualComponent }
-];
